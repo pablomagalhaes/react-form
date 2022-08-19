812c3f639679ac873cb185f6e430786f7b429863
Thiago Magalhaes <pablothiagomagalhaes@iMac-de-Thiago.local>
2022-08-19 14:26:56 -0300

    First Commit

---
+++ src/components/Form/styles.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+import { btnReset, v } from "../../styles/variables";
+
+export const SForm = styled.form`
+    width: 100%;
+    background: ${({ theme }) => theme.bg2};
+    border-radius: ${v.borderRadius};
+    padding: ${v.mdSpacing};
+`;
+
+export const SFormTitle = styled.span`
+    font-size: 24px;
+    font-weight: 600;
+`;
+
+export const SFormControl = styled.div`
+    :first-of-type {
+        margin-top: ${v.mdSpacing};
+    }
+    :not(:last-of-type) {
+        margin-bottom: ${v.smSpacing};
+    }
+`;
+
+export const SLabel = styled.label`
+    display: block;
+    font-size: 14px;
+    font-weight: 600;
+    margin-left: 4px;
+    margin-bottom: calc(${v.smSpacing} / 4);
+`;
+
+export const SInput = styled.input`
+    outline: none;
+    border: 1px solid ${({ theme }) => theme.textFade};
+    width: 100%;
+    padding: ${v.smSpacing};
+    font-size: 14px;
+    border-radius: ${v.borderRadius};
+`;
+
+export const SButton = styled.button`
+    ${btnReset};
+    width: 100%;
+    background: ${({ theme }) => theme.bgSecondary};
+    color: ${({ theme }) => theme.textPrimary};
+    padding: ${v.smSpacing};
+    display: flex;
+    justify-content: center;
+    border-radius: ${v.borderRadius};
+    margin-top: ${v.mdSpacing};
+    cursor: pointer;
+`;
---
+++ src/pages/Register.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { SFixedContainer } from "../components/Containers/styles";
+import Form from "../components/Form/Form";
+
+const Register = () => {
+    const onSubmitHandler = (form, callback) => {
+        console.log("Register Submitted:", form);
+        callback();
+    };
+
+    return (
+        <SFixedContainer size={275}>
+            <Form
+                title={"Register"}
+                formArr={formArr}
+                submitBtn={"Register"}
+                onSubmit={onSubmitHandler}
+            />
+        </SFixedContainer>
+    );
+};
+
+const formArr = [
+    {
+        label: "Name",
+        name: "name",
+        type: "text",
+    },
+    {
+        label: "Email",
+        name: "email",
+        type: "text",
+    },
+
+    {
+        label: "Password",
+        name: "password",
+        type: "password",
+    },
+    {
+        label: "Confirm Password",
+        name: "confirmPassword",
+        type: "password",
+    },
+];
+
+export default Register;
